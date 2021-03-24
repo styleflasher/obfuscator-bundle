4f68c6cabd6e197057d01647f881a6e1c304e501
Salih Candir <salih.candir@styleflasher.at>
2021-03-24 16:24:05 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# StyleflasherEmailObfuscation
+
+``` yml
+// config/bundles.php
+return [
+    // ...
+    App\Acme\TestBundle\AcmeTestBundle::class => ['all' => true],
+];
+```
---
+++ src/Styleflasher/ObfuscatorBundle/StyleflasherObfuscatorBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Styleflasher\ObfuscatorBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StyleflasherObfuscatorBundle extends Bundle
+{
+
+}
